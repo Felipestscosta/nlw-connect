95e118bc7f6d75a99dcb28923a8541b79ab7f397
Felipe <felipestscosta@gmail.com>
2025-02-19 20:26:58 -0300

    feat: tela de invite

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,15 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface ButtonIconProps extends ComponentProps<'button'> {}
 
-export function ButtonIcon(props: ButtonIconProps) {
+export function IconButton({ className, ...props }: ButtonIconProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue transition-colors duration-300 hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   )
